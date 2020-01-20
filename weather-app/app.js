1b5d94c06c9c044287993e0547af4e24ab2533c1
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-01-20 15:17:12 -0800

    use of Weather API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,10 +1,9 @@
 const request = require("request")
 
-const url = "https://api.darksky.net/forecast/c6fc2eac3ea5647627be1ffec472eb2f/37.8267,-122.4233"
+const url = "https://api.darksky.net/forecast/c6fc2eac3ea5647627be1ffec472eb2f/37.8267,-122.4233?units=si"
 
-request({url: url}, (error, response) => {
+request({url: url, json: true}, (error, response) => {
 
-    const data = JSON.parse(response.body)
-
-    console.log(data.currently)
+    console.log(response.body.daily.data[0].summary + " It is currently " + response.body.currently.temperature + " degrees out." + " There is " +
+        (response.body.currently.precipProbability * 100) + " % " + "chance of rain")
 })
