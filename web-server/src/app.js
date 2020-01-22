13e2405c3edc8c8df3b1fa7cd29be6462063c038
Saurabh Jaiswal <saurabhchris1@gmail.com>
2020-01-22 10:47:29 -0800

    Adding Handel Bars Template engine

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,24 +1,35 @@
+const path = require('path')
 const express = require('express')
-
 const app = express()
 
-app.get('', (req, res)=>{
-    res.send("Hello Express")
+const publicDirPath = path.join(__dirname, '../public')
+
+app.set('view engine', 'hbs')
+app.use(express.static(publicDirPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Saurabh Jaiswal'
+    })
 })
 
-app.get('/help', (req, res)=>{
-    res.send('Help Page')
+app.get('/about', (req,res) => {
+    res.render('about', {
+        title: 'About me',
+        name: 'Saurabh Jaiswal'
+    })
 })
 
-app.get('/about', (req, res)=>{
-    res.send('about page')
-})
-
-app.get('/weather', (req, res)=>{
-    res.send('View Weather')
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help',
+        name: 'Saurabh Jaiswal'
+    })
 })
 
 app.listen(3000, () => {
     console.log("Server is up on port 3000")
 })
+
 
